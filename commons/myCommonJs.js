5be7f3e80c6d99a87096dec1cfd2866d0cfd23ef
landinialejandro <landinialejandro@hotmail.com>
2020-09-26 15:29:05 -0300

    fusers list

--- commons/myCommonJs.js
+++ commons/myCommonJs.js
@@ -455,29 +455,28 @@ function addTabs(id = "new", tabs = []) {
  * @param {string} t - table name destiny.
  */
 function users_dropdown(f, t) {
-    var $selectField = jQuery('#' + f + '').hide();
-    $selectField.closest('div').append(jQuery('<span/>', { id: 's2_users_' + f }));
+    var $selectField = $j('#' + f + '').hide();
+    var $span = $j('<span/>', { id: 's2_users_' + f });
+    $selectField.closest('div').append($span);
     var val = $selectField.val();
 
-    jQuery('[id=s2_users_' + f + ']').select2({
+    $span.select2({
         width: '100%',
-        formatNoMatches: function(term) { /* */
-            return 'No matches found!';
-        },
+        formatNoMatches: function(term) { return 'No matches found!'; },
         minimumResultsForSearch: 5,
         loadMorePadding: 200,
-        escapeMarkup: function(m) { /* */ return m; },
+        escapeMarkup: function(m) { return m; },
         ajax: {
             url: 'admin/getUsers.php',
             dataType: 'json',
             cache: true,
-            data: function(term, page) { /* */ return { s: term, p: page, t: t }; },
-            results: function(resp, page) { /* */ return resp; }
+            data: function(term, page) { return { s: term, p: page, t: t }; },
+            results: function(resp, page) { return resp; }
         }
     }).on('change', function(e) {
-        jQuery('[name="' + f + '"]').val(e.added.id);
+        $j('[name="' + f + '"]').val(e.added.id);
     });
     if (val) {
-        jQuery('[id=s2_users_' + f + ']').select2('data', { text: val, id: val });
+        $span.select2('data', { text: val, id: val });
     }
 }
